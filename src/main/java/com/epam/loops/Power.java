20c47d595d923ac8035c1afa0ed280b48667ee31
samandarmaxsutov <gitsamandar1@gmail.com>
2023-12-09 10:32:30 +0500

    Task 3. Power

--- src/main/java/com/epam/loops/Power.java
+++ src/main/java/com/epam/loops/Power.java
@@ -3,7 +3,11 @@ package com.epam.loops;
 public class Power {
 
     public void printPower(int numberToPrint, int power) {
-
+        int a=numberToPrint;
+        for (int i = 1; i <power; i++) {
+            a*=numberToPrint;
+        }
+        System.out.println(a);
     }
 
     public static void main(String[] args) {
