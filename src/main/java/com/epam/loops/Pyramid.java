d896f9532987b8ad6285a33829f063a1fd200be8
samandarmaxsutov <gitsamandar1@gmail.com>
2023-12-09 10:05:22 +0500

    Task 2. Pyramid

--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -3,11 +3,22 @@ package com.epam.loops;
 public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength - i; j++) {
+                System.out.print(" ");
+            }
 
+            for (int k = 1; k <= i; k++) {
+                System.out.print((i-k+1));
+            }
+            for (int k = 2; k <= i; k++) {
+                System.out.print((k));
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
         new Pyramid().printPyramid(7);
     }
-
 }
