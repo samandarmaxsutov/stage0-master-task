7e6f85c7275fa5add6a7247338af3893ad24f9e2
samandarmaxsutov <gitsamandar1@gmail.com>
2023-12-09 08:41:21 +0500

    Task 1. Season determiner.

--- src/main/java/com/epam/conditions/SeasonDeterminer.java
+++ src/main/java/com/epam/conditions/SeasonDeterminer.java
@@ -3,6 +3,19 @@ package com.epam.conditions;
 public class SeasonDeterminer {
 
     public void tellTheSeason(int monthNumber) {
+        String season = switch (monthNumber) {
+            case 1, 2, 12 -> "Winter";
+            case 4, 5, 3 -> "Spring";
+            case 7, 8, 6 -> "Summer";
+            case 10, 11, 9  -> "Autumn";
+            default -> "Wrong month number";
+        };
+
+        System.out.println(season);
+    }
+    public static void main(String[] args) {
+        SeasonDeterminer seasonDeterminer = new SeasonDeterminer();
+        seasonDeterminer.tellTheSeason(12);
     }
 
 }
