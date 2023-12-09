6d8c936e4f53311371a4984cadef97f49dd99169
samandarmaxsutov <gitsamandar1@gmail.com>
2023-12-09 08:45:43 +0500

    Task 2. Triangle sides validator.

--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -2,8 +2,15 @@ package com.epam.conditions;
 
 public class TriangleSidesValidator {
 
+    private boolean checkTriangleSides(double a,double b,double c){
+        return a+b>c&&a+c>b&&b+c>a;
+    }
     public void validate(double firstSide, double secondSide, double thirdSide) {
-
+        if (checkTriangleSides(firstSide, secondSide, thirdSide)) {
+            System.out.println("this is a valid triangle");
+        } else {
+            System.out.println("it's not a triangle");
+        }
     }
 
 }
