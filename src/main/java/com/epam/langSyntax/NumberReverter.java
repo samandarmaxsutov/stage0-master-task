19047547417a0ae40236934f5128f11b2397dfb4
samandarmaxsutov <gitsamandar1@gmail.com>
2023-12-09 08:21:43 +0500

    Task 1. Numbers reverter.

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -2,7 +2,20 @@ package com.epam.langSyntax;
 
 public class NumberReverter {
 
-    public void revert(int number) {
+    public void revert(int number) {//489
+        int rNumber = 0;// rNumber = 0
+
+        while (number != 0) { // 489!=0 // 48!=0
+            int digit = number % 10;//digit = 9 // = 8// 4
+            rNumber = rNumber*10 + digit; //rNumber = 0*10+9 // 9*10+8 // 98*10+4
+            number /= 10; //number =48 //= 4 // = 0
+        }
+
+        System.out.println(rNumber); // 984
     }
 
+    public static void main(String[] args) {
+        NumberReverter numberReverter = new NumberReverter();
+        numberReverter.revert(489); // Replace 489 with the desired 3-digit number
+    }
 }
