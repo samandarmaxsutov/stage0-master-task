45de05b74780ec8408995f76686b777ecf8f9f24
samandarmaxsutov <gitsamandar1@gmail.com>
2023-12-09 08:32:07 +0500

    Task 2. Digits sum calculator.

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,17 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        int sum =0;
+        while (number!= 0) {
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+        System.out.println(sum);
     }
 
+    public static void main(String[] args) {
+        DigitsSumCalculator digitsSumCalculator = new DigitsSumCalculator();
+        digitsSumCalculator.calculateSum(489);
+    }
 }
