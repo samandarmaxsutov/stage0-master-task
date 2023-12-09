120e27b2b22ffe9cff6eb44f812550fa36c38768
samandarmaxsutov <gitsamandar1@gmail.com>
2023-12-09 10:36:28 +0500

    Task 4. Half-Pyramid

--- src/main/java/com/epam/loops/HalfPyramid.java
+++ src/main/java/com/epam/loops/HalfPyramid.java
@@ -3,6 +3,15 @@ package com.epam.loops;
 public class HalfPyramid {
 
     public void printHalfPyramid(int cathetusLength) {
+        for (int i = 1; i <=cathetusLength ; i++) {
+            for (int j = 1; j <=cathetusLength-i ; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
     }
 
